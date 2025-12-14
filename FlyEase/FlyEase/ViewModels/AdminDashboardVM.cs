776536132fd271a22eeb7197765090d90b072d6c
Dgoh1113 <dgoh2004@gmail.com>
2025-12-15 04:41:11 +0800

    add searching and filter in admindashboard, discount will fix tmr

--- FlyEase/FlyEase/ViewModels/AdminDashboardVM.cs
+++ FlyEase/FlyEase/ViewModels/AdminDashboardVM.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using FlyEase.Data;
+using System.Collections.Generic;
 
 namespace FlyEase.ViewModels
 {
@@ -8,31 +9,31 @@ namespace FlyEase.ViewModels
     {
         public int TotalUsers { get; set; }
         public int TotalBookings { get; set; }
-
         public int PendingBookings { get; set; }
         public decimal TotalRevenue { get; set; }
         public List<Booking> RecentBookings { get; set; } = new List<Booking>();
         public List<Package> LowStockPackages { get; set; } = new List<Package>();
 
-        // === NEW: For Chart.js ===
+        // For Chart.js
         public List<string> PackageNames { get; set; } = new List<string>();
         public List<double> PackageRatings { get; set; } = new List<double>();
-
-        public List<string> RevenueMonths { get; set; } = new List<string>(); // e.g., ["Jan", "Feb", "Mar"]
-        public List<decimal> RevenueValues { get; set; } = new List<decimal>(); // e.g., [1200, 3500, 4200]  
+        public List<string> RevenueMonths { get; set; } = new List<string>();
+        public List<decimal> RevenueValues { get; set; } = new List<decimal>();
         public List<int> PackageBookingCounts { get; set; } = new List<int>();
         public int CompletedBookingsCount { get; set; }
         public int CancelledBookingsCount { get; set; }
         public int PendingBookingsCount { get; set; }
-
     }
-
 
     // 2. For the Users Management Tab
     public class UsersPageVM
     {
         public List<User> Users { get; set; } = new List<User>();
         public User CurrentUser { get; set; } = new User();
+
+        // Search & Filter
+        public string? SearchTerm { get; set; }
+        public string? RoleFilter { get; set; }
     }
 
     // 3. For the Bookings Management Tab
@@ -40,18 +41,32 @@ namespace FlyEase.ViewModels
     {
         public List<Booking> Bookings { get; set; } = new List<Booking>();
         public Booking CurrentBooking { get; set; } = new Booking();
+
+        // Search
+        public string? SearchTerm { get; set; }
+        public string? StatusFilter { get; set; } // Renamed from just 'status' param for clarity
     }
 
     // 4. For the Packages Management Tab
     public class PackagesPageVM
     {
-        // The List for the Table
         public List<Package> Packages { get; set; } = new List<Package>();
-
-        // The Data for the Dropdowns
         public List<PackageCategory> Categories { get; set; } = new List<PackageCategory>();
-
-        // The Single Object for the Create/Edit Form
         public Package CurrentPackage { get; set; } = new Package();
+
+        // Search
+        public string? SearchTerm { get; set; }
+    }
+
+    // 5. For the Discounts Management Tab
+    public class DiscountsPageVM
+    {
+        public List<DiscountType> Discounts { get; set; } = new List<DiscountType>();
+
+        // The object used for Creating or Editing a specific discount
+        public DiscountType CurrentDiscount { get; set; } = new DiscountType();
+
+        // Search
+        public string? SearchTerm { get; set; }
     }
 }
--- FlyEase/FlyEase/ViewModels/DiscountPageVM.cs
+++ FlyEase/FlyEase/ViewModels/DiscountPageVM.cs
@@ -1,14 +1,12 @@
 ﻿using FlyEase.Data;
-using System.Collections.Generic;
 
-namespace FlyEase.ViewModels
+public class DiscountsPageVM
 {
-    public class DiscountsPageVM
-    {
-        // List of all discounts to display in the table
-        public List<DiscountType> Discounts { get; set; } = new List<DiscountType>();
+    public List<DiscountType> Discounts { get; set; } = new List<DiscountType>();
 
-        // The object used for Creating or Editing a specific discount
-        public DiscountType CurrentDiscount { get; set; } = new DiscountType();
-    }
+    // The object used for Creating or Editing a specific discount
+    public DiscountType CurrentDiscount { get; set; } = new DiscountType();
+
+    // Search
+    public string? SearchTerm { get; set; }
 }
